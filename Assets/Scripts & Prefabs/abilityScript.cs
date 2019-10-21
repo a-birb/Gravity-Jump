5c2b982daa397ad774354740ef92180f6f47c00f
a-birb <56260898+a-birb@users.noreply.github.com>
2019-10-21 08:21:21 -0400

    updates

--- Assets/Scripts & Prefabs/abilityScript.cs
+++ Assets/Scripts & Prefabs/abilityScript.cs
@@ -6,20 +6,36 @@ public class abilityScript : MonoBehaviour {
 
     Rigidbody2D r;
     float jumpcd;
+    float firecd;
+    public int jumpKey = 1;
+    public int fireKey = 0;
 
     void Start() {
         jumpcd = Time.time;
+        firecd = Time.time;
         r = GetComponent<Rigidbody2D>();
 
     }
     
     void Update() {
 
-        if(Input.GetMouseButtonDown(0) == true && jumpcd <= Time.time) {
+        Jump();
+        Fire();
+
+    }
+
+    void Jump() {
+        if(Input.GetMouseButtonDown(jumpKey) == true && jumpcd <= Time.time) {
             jumpcd = Time.time + 1f;
             r.AddRelativeForce(new Vector2(8000f,0));
         }
+    }
 
+    void Fire() {
+        if(Input.GetMouseButton(fireKey) == true && firecd <= Time.time) {
+            firecd = Time.time + 0.08f;
+            GameObject.Instantiate();
+        }
     }
 }
 
